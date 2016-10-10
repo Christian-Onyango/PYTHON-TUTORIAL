ea78079be2d055caab41b21a1f1ab60901702571
christian onyango <christian onyango>
2016-10-10 12:53:37 +0300

    Modified fibonacci code

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,21 @@
-def F(n):
-    if n == 0: return 0
-    elif n == 1: return 1
-    else: return F(n-1)+F(n-2)
-print (F(20))
+nterms = 10
+n1 = 0
+n2 = 1
+count = 2
+
+
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+else:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1,",",n2,end=', ')
+   while count < nterms:
+       nth = n1 + n2
+       print(nth,end=' , ')
+      
+       n1 = n2
+       n2 = nth
+       count += 1
